ac4150d15b527e74f22b02dc424efb28cf655be8
Patrick Lühne <patrick-github@luehne.de>
2016-10-07 15:42:35 +0200

    Switched from Google Test to Catch.

--- tests/TestIntegrityConstraints.cpp
+++ tests/TestIntegrityConstraints.cpp
@@ -1,23 +1,23 @@
-#include <gtest/gtest.h>
+#include <catch.hpp>
 
 #include <ginkgo/solving/Constraint.h>
 #include <ginkgo/solving/GeneralizedConstraint.h>
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-TEST(ConstraintTests, Repetition)
+TEST_CASE("Duplicate literals in constraints are eliminated", "[constraints]")
 {
 	ginkgo::SymbolTable symbolTable;
 	ginkgo::Constraint a(0, ":- a, b, b, a, b, a(b(c(d(e)))), a(b(c(d(e)))), a(b(c(d(e)))).", symbolTable);
 	ginkgo::Constraint b(0, ":- a, b, a(b(c(d(e)))).", symbolTable);
 
-	EXPECT_EQ(a.literals().size(), 3);
-	EXPECT_EQ(b.literals().size(), 3);
+	REQUIRE(a.literals().size() == 3);
+	REQUIRE(b.literals().size() == 3);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-TEST(ConstraintTests, TimeRange)
+TEST_CASE("The time arguments of temporal literals are parsed correctly", "[constraints]")
 {
 	ginkgo::SymbolTable symbolTable;
 	ginkgo::Constraint a(0, ":- holds(a, 0), holds(b, 0), holds(c, 0).", symbolTable);
@@ -25,46 +25,46 @@ TEST(ConstraintTests, TimeRange)
 	ginkgo::Constraint c(0, ":- holds(a, 100), holds(b, 1), holds(c, 50).", symbolTable);
 	ginkgo::Constraint d(0, ":- holds(a, 12), holds(b, 8), holds(c, 100).", symbolTable);
 
-	EXPECT_EQ(a.timeRange(), std::make_tuple(0, 0));
-	EXPECT_EQ(b.timeRange(), std::make_tuple(0, 2));
-	EXPECT_EQ(c.timeRange(), std::make_tuple(1, 100));
-	EXPECT_EQ(d.timeRange(), std::make_tuple(8, 100));
+	REQUIRE(a.timeRange() == std::make_tuple(0, 0));
+	REQUIRE(b.timeRange() == std::make_tuple(0, 2));
+	REQUIRE(c.timeRange() == std::make_tuple(1, 100));
+	REQUIRE(d.timeRange() == std::make_tuple(8, 100));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-TEST(ConstraintTests, ContainsIdentifier)
+TEST_CASE("Literal identifiers are collected correctly", "[constraints]")
 {
 	ginkgo::SymbolTable symbolTable;
 	ginkgo::Constraint a(0, ":- a, b, c, terminal, holds(f, 7), not apply(t, 1).", symbolTable);
 
-	EXPECT_TRUE(a.containsIdentifier("a"));
-	EXPECT_TRUE(a.containsIdentifier("terminal"));
-	EXPECT_TRUE(a.containsIdentifier("holds"));
-	EXPECT_FALSE(a.containsIdentifier("e"));
-	EXPECT_FALSE(a.containsIdentifier("f"));
-	EXPECT_FALSE(a.containsIdentifier("7"));
-	EXPECT_TRUE(a.containsIdentifier("apply"));
+	REQUIRE(a.containsIdentifier("a"));
+	REQUIRE(a.containsIdentifier("terminal"));
+	REQUIRE(a.containsIdentifier("holds"));
+	REQUIRE_FALSE(a.containsIdentifier("e"));
+	REQUIRE_FALSE(a.containsIdentifier("f"));
+	REQUIRE_FALSE(a.containsIdentifier("7"));
+	REQUIRE(a.containsIdentifier("apply"));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-TEST(ConstraintTests, Generalizing)
+TEST_CASE("Constraints are correctly generalized and subsumed", "[constraints]")
 {
 	ginkgo::SymbolTable symbolTable;
 	auto a = std::make_shared<ginkgo::Constraint>(0, ":- holds(a, 0), holds(b, 0), holds(c, 0).", symbolTable);
 	auto b = std::make_shared<ginkgo::Constraint>(0, ":- holds(a, 5), holds(b, 5), holds(c, 5).", symbolTable);
 	auto c = std::make_shared<ginkgo::Constraint>(0, ":- holds(a, 4), holds(b, 5), holds(c, 7).", symbolTable);
 
-	EXPECT_TRUE(ginkgo::GeneralizedConstraint(b).subsumes(*a));
-	EXPECT_TRUE(ginkgo::GeneralizedConstraint(a).subsumes(*b));
-	EXPECT_FALSE(ginkgo::GeneralizedConstraint(a).subsumes(*c));
-	EXPECT_FALSE(ginkgo::GeneralizedConstraint(c).subsumes(*a));
+	REQUIRE(ginkgo::GeneralizedConstraint(b).subsumes(*a));
+	REQUIRE(ginkgo::GeneralizedConstraint(a).subsumes(*b));
+	REQUIRE_FALSE(ginkgo::GeneralizedConstraint(a).subsumes(*c));
+	REQUIRE_FALSE(ginkgo::GeneralizedConstraint(c).subsumes(*a));
 
 	std::stringstream outputB;
 	ginkgo::GeneralizedConstraint(a).print(outputB);
 
-	EXPECT_EQ(outputB.str(), ":- time(T), holds(a, T), holds(b, T), holds(c, T).");
+	REQUIRE(outputB.str() == ":- time(T), holds(a, T), holds(b, T), holds(c, T).");
 
 	auto d = std::make_shared<ginkgo::Constraint>(0, ":- apply(a, 6), holds(b, 6), holds(c, 6).", symbolTable);
 
@@ -72,40 +72,40 @@ TEST(ConstraintTests, Generalizing)
 	std::stringstream outputD;
 	ginkgo::GeneralizedConstraint(d).print(outputD);
 
-	EXPECT_EQ(outputD.str(), ":- time(T), time(T+1), apply(a, T+1), holds(b, T+1), holds(c, T+1).");
+	REQUIRE(outputD.str() == ":- time(T), time(T+1), apply(a, T+1), holds(b, T+1), holds(c, T+1).");
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-TEST(ConstraintTests, LiteralElimination)
+TEST_CASE("Literals are eliminated as specified", "[constraints]")
 {
 	ginkgo::SymbolTable symbolTable;
 	ginkgo::Constraint a(0, ":- a, b, c, d, e.", symbolTable);
 	ginkgo::Constraint b(0, ":- a, b, c, e.", symbolTable);
 	ginkgo::Constraint c(0, ":- b, c, e.", symbolTable);
 
-	EXPECT_TRUE(a.withoutLiterals(3)->subsumes(b));
-	EXPECT_TRUE(b.subsumes(*a.withoutLiterals(3)));
+	REQUIRE(a.withoutLiterals(3)->subsumes(b));
+	REQUIRE(b.subsumes(*a.withoutLiterals(3)));
 
-	EXPECT_TRUE(b.withoutLiterals(0)->subsumes(c));
-	EXPECT_TRUE(c.subsumes(*b.withoutLiterals(0)));
+	REQUIRE(b.withoutLiterals(0)->subsumes(c));
+	REQUIRE(c.subsumes(*b.withoutLiterals(0)));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-TEST(ConstraintTests, SubsumptionIdentity)
+TEST_CASE("Constraits subsume themselves", "[constraints]")
 {
 	ginkgo::SymbolTable symbolTable;
 	ginkgo::Constraint a(0, ":- a, b, c, d, e.", symbolTable);
 	ginkgo::Constraint b(0, ":- a, b, not c, d, not e.", symbolTable);
 
-	EXPECT_TRUE(a.subsumes(a));
-	EXPECT_TRUE(b.subsumes(b));
+	REQUIRE(a.subsumes(a));
+	REQUIRE(b.subsumes(b));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-TEST(ConstraintTests, SubsumptionSimple)
+TEST_CASE("Constraints are correctly subsumed by other constraints", "[constraints]")
 {
 	ginkgo::SymbolTable symbolTable;
 	ginkgo::Constraint a(0, ":- a, b, c, d, e.", symbolTable);
@@ -114,59 +114,59 @@ TEST(ConstraintTests, SubsumptionSimple)
 	ginkgo::Constraint d(0, ":- a, b, c, f.", symbolTable);
 	ginkgo::Constraint e(0, ":- e, d, a, b, c.", symbolTable);
 
-	EXPECT_TRUE(b.subsumes(a));
-	EXPECT_FALSE(a.subsumes(b));
+	REQUIRE(b.subsumes(a));
+	REQUIRE_FALSE(a.subsumes(b));
 
-	EXPECT_TRUE(c.subsumes(a));
-	EXPECT_FALSE(a.subsumes(c));
+	REQUIRE(c.subsumes(a));
+	REQUIRE_FALSE(a.subsumes(c));
 
-	EXPECT_FALSE(d.subsumes(a));
-	EXPECT_FALSE(a.subsumes(d));
+	REQUIRE_FALSE(d.subsumes(a));
+	REQUIRE_FALSE(a.subsumes(d));
 
-	EXPECT_TRUE(e.subsumes(a));
-	EXPECT_TRUE(a.subsumes(e));
+	REQUIRE(e.subsumes(a));
+	REQUIRE(a.subsumes(e));
 
-	EXPECT_TRUE(c.subsumes(b));
-	EXPECT_FALSE(b.subsumes(c));
+	REQUIRE(c.subsumes(b));
+	REQUIRE_FALSE(b.subsumes(c));
 
-	EXPECT_FALSE(d.subsumes(b));
-	EXPECT_FALSE(b.subsumes(d));
+	REQUIRE_FALSE(d.subsumes(b));
+	REQUIRE_FALSE(b.subsumes(d));
 
-	EXPECT_TRUE(b.subsumes(e));
-	EXPECT_FALSE(e.subsumes(b));
+	REQUIRE(b.subsumes(e));
+	REQUIRE_FALSE(e.subsumes(b));
 
-	EXPECT_FALSE(d.subsumes(c));
-	EXPECT_FALSE(c.subsumes(d));
+	REQUIRE_FALSE(d.subsumes(c));
+	REQUIRE_FALSE(c.subsumes(d));
 
-	EXPECT_FALSE(e.subsumes(c));
-	EXPECT_TRUE(c.subsumes(e));
+	REQUIRE_FALSE(e.subsumes(c));
+	REQUIRE(c.subsumes(e));
 
-	EXPECT_FALSE(e.subsumes(d));
-	EXPECT_FALSE(d.subsumes(e));
+	REQUIRE_FALSE(e.subsumes(d));
+	REQUIRE_FALSE(d.subsumes(e));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-TEST(ConstraintTests, Negation)
+TEST_CASE("Constraint subsumption is negation-sensitive", "[constraints]")
 {
 	ginkgo::SymbolTable symbolTable;
 	ginkgo::Constraint a(0, ":- a, not b, c, d, e.", symbolTable);
 	ginkgo::Constraint b(0, ":- a, not b, c, d, not e.", symbolTable);
 	ginkgo::Constraint c(0, ":- a, not b, c, d.", symbolTable);
 
-	EXPECT_TRUE(c.subsumes(a));
-	EXPECT_TRUE(c.subsumes(b));
+	REQUIRE(c.subsumes(a));
+	REQUIRE(c.subsumes(b));
 
-	EXPECT_FALSE(a.subsumes(c));
-	EXPECT_FALSE(b.subsumes(c));
+	REQUIRE_FALSE(a.subsumes(c));
+	REQUIRE_FALSE(b.subsumes(c));
 
-	EXPECT_FALSE(b.subsumes(a));
-	EXPECT_FALSE(a.subsumes(b));
+	REQUIRE_FALSE(b.subsumes(a));
+	REQUIRE_FALSE(a.subsumes(b));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-TEST(ConstraintTests, SubsumptionGeneralized)
+TEST_CASE("Generalized constraints are correctly subsumed by other generalized constraints", "[constraints]")
 {
 	ginkgo::SymbolTable symbolTable;
 
@@ -188,34 +188,34 @@ TEST(ConstraintTests, SubsumptionGeneralized)
 	ginkgo::Constraint l(0, ":- holds(a, 60), holds(b, 60), holds(c, 60), holds(spam, 60).", symbolTable);
 	ginkgo::Constraint m(0, ":- holds(a, 60), holds(b, 60), holds(c, 60), holds(spam, 80).", symbolTable);
 
-	EXPECT_TRUE(generalizedA.subsumes(c));
-	EXPECT_FALSE(generalizedA.subsumes(d));
-	EXPECT_TRUE(generalizedA.subsumes(e));
-	EXPECT_FALSE(generalizedA.subsumes(f));
-	EXPECT_FALSE(generalizedA.subsumes(g));
-	EXPECT_TRUE(generalizedA.subsumes(h));
-	EXPECT_TRUE(generalizedA.subsumes(i));
-	EXPECT_FALSE(generalizedA.subsumes(j));
-	EXPECT_TRUE(generalizedA.subsumes(k));
-	EXPECT_TRUE(generalizedA.subsumes(l));
-	EXPECT_TRUE(generalizedA.subsumes(m));
+	REQUIRE(generalizedA.subsumes(c));
+	REQUIRE_FALSE(generalizedA.subsumes(d));
+	REQUIRE(generalizedA.subsumes(e));
+	REQUIRE_FALSE(generalizedA.subsumes(f));
+	REQUIRE_FALSE(generalizedA.subsumes(g));
+	REQUIRE(generalizedA.subsumes(h));
+	REQUIRE(generalizedA.subsumes(i));
+	REQUIRE_FALSE(generalizedA.subsumes(j));
+	REQUIRE(generalizedA.subsumes(k));
+	REQUIRE(generalizedA.subsumes(l));
+	REQUIRE(generalizedA.subsumes(m));
 
-	EXPECT_FALSE(generalizedB.subsumes(c));
-	EXPECT_FALSE(generalizedB.subsumes(d));
-	EXPECT_FALSE(generalizedB.subsumes(e));
-	EXPECT_TRUE(generalizedB.subsumes(f));
-	EXPECT_TRUE(generalizedB.subsumes(g));
-	EXPECT_FALSE(generalizedB.subsumes(h));
-	EXPECT_FALSE(generalizedB.subsumes(i));
-	EXPECT_TRUE(generalizedB.subsumes(j));
-	EXPECT_FALSE(generalizedB.subsumes(k));
-	EXPECT_FALSE(generalizedB.subsumes(l));
-	EXPECT_FALSE(generalizedB.subsumes(m));
+	REQUIRE_FALSE(generalizedB.subsumes(c));
+	REQUIRE_FALSE(generalizedB.subsumes(d));
+	REQUIRE_FALSE(generalizedB.subsumes(e));
+	REQUIRE(generalizedB.subsumes(f));
+	REQUIRE(generalizedB.subsumes(g));
+	REQUIRE_FALSE(generalizedB.subsumes(h));
+	REQUIRE_FALSE(generalizedB.subsumes(i));
+	REQUIRE(generalizedB.subsumes(j));
+	REQUIRE_FALSE(generalizedB.subsumes(k));
+	REQUIRE_FALSE(generalizedB.subsumes(l));
+	REQUIRE_FALSE(generalizedB.subsumes(m));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-TEST(ConstraintTests, SubsumptionGeneralizedWithActions)
+TEST_CASE("Generalized constraint subsumption works with actions", "[constraints]")
 {
 	ginkgo::SymbolTable symbolTable;
 
@@ -227,7 +227,7 @@ TEST(ConstraintTests, SubsumptionGeneralizedWithActions)
 	ginkgo::Constraint c(0, ":- apply(a, 34), holds(b, 35), holds(c, 37), holds(spam, 37).", symbolTable);
 	ginkgo::Constraint d(0, ":- apply(a, 8), holds(b, 6), holds(c, 8), holds(spam, 37).", symbolTable);
 
-	EXPECT_TRUE(generalizedA.subsumes(b));
-	EXPECT_TRUE(generalizedA.subsumes(c));
-	EXPECT_FALSE(generalizedA.subsumes(d));
+	REQUIRE(generalizedA.subsumes(b));
+	REQUIRE(generalizedA.subsumes(c));
+	REQUIRE_FALSE(generalizedA.subsumes(d));
 }
